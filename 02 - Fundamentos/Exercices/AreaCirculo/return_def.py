2fbe3e7faf4d25e520ccb51a285632bf95e73aff
Mendescaua <cauamende1703@gmail.com>
2025-08-26 16:38:07 -0300

    day 3

---
+++ 02 - Fundamentos/Exercices/AreaCirculo/return_def.py
@@ -0,0 +1,11 @@
+# Função que retorna o quadrado de um número
+from math import pi
+
+
+def circulo(raio):
+    return round(pi * raio ** 2, 2)
+
+# Exemplo de uso:
+# import return_def
+# 
+# print(f"Funcao {return_def.circulo(10)}")
